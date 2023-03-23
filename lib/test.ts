07cb9a87d3704fda1f99fdcf15b970069b300e0f
drakulah <118066433+drakulah@users.noreply.github.com>
2023-03-23 08:58:47 +0545

    Delete test.ts

--- lib/test.ts
+++
@@ -1,9 +0,0 @@
-import YouTubeMusic from './youtubemusic/YouTubeMusic.ts'
-
-const ytm = new YouTubeMusic()
-  .withVisitorId('CgtBTXhfYy1IVGc1cyi19-SeBg%3D%3D')
-
-const res = await ytm.upNext('RDAMVMpsuRGfAaju4', 'psuRGfAaju4')
-if (res) {
-  Deno.writeTextFileSync('1234.jsonc', JSON.stringify(res))
-}
